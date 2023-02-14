fc787e10a9635b9ca057b84dd4bf90024e4b6249
Eidan Khan <eidan.khan@360factors.com>
2023-02-15 01:41:18 +0500

    init project with jwt implementation

---
+++ src/test/java/com/sprinboot/jwt/app/SpringBootJwtApplicationTests.java
@@ -0,0 +1,12 @@
+package com.sprinboot.jwt.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
